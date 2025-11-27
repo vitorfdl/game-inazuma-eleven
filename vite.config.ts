71341c479bf16999a8a0792b6dea3ef07cb98935
Vitor <vitor@ferreiradelima.com>
2025-11-27 06:56:18 -0300

    4th commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 
-const proxyConfig = {
-	"/api/grindosaur": {
-		target: "https://www.grindosaur.com",
-		changeOrigin: true,
-		rewrite: (path: any) => path.replace(/^\/api\/grindosaur/, ""),
-	},
-} as const;
-
 // https://vite.dev/config/
 export default defineConfig({
-	// Base path for GitHub Pages under /game-digimon-story-time/
-	base: "/game-inazuma-eleven-guide/",
+	base: "/game-inazuma-eleven/",
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
-	},
-	server: {
-		open: true,
-		proxy: proxyConfig,
-	},
-	preview: {
-		proxy: proxyConfig,
-	},
+	}
 });
